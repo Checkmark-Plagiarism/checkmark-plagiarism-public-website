af73bdd424b7e2a9e34a1d4238e64c2a0ede7207
nmark <18markn@gmail.com>
2025-11-04 08:47:49 -0800

    remove video button

--- src/sections/home/hero-section.tsx
+++ src/sections/home/hero-section.tsx
@@ -89,12 +89,12 @@ export const Hero = () => {
 
             {/* Modal Trigger */}
             <Dialog>
-              <DialogTrigger asChild>
+              {/* <DialogTrigger asChild>
                 <Button variant="outline-white" size="xl" className="bg-brand-600">
                   <Play className="mr-2" />
                   Watch 60-sec Overview
                 </Button>
-              </DialogTrigger>
+              </DialogTrigger> */}
               <DialogContent className="max-w-4xl p-0 overflow-hidden">
                 <DialogHeader className="px-6 pt-6">
                   <DialogTitle className="text-xl">Product Overview</DialogTitle>
