24a157b95649b3ca43403858d28d69585beceef5
JaronKing <jaronjking@gmail.com>
2026-02-21 08:29:36 -0800

    Updates

--- src/app/lander/canvas/page.tsx
+++ src/app/lander/canvas/page.tsx
@@ -4,6 +4,7 @@ import { CanvasHowItWorks } from "@/sections/lander/canvas/how-it-works";
 import { CanvasTestimonial } from "@/sections/lander/canvas/testimonials";
 import { CanvasCTA } from "@/sections/lander/canvas/cta";
 import { CanvasSignupForm } from "@/sections/lander/canvas/signup-form";
+import { CanvasDemo } from "@/sections/lander/canvas/demo";
 
 const CanvasIntegration = () => {
     return (
@@ -12,6 +13,7 @@ const CanvasIntegration = () => {
                 <CanvasHero />
                 <CanvasFeatures />
                 <CanvasHowItWorks />
+                <CanvasDemo />
                 <CanvasSignupForm />
                 <CanvasTestimonial />
                 <CanvasCTA />
